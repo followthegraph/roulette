6528e8ff13b9fdfabdbbc10e48fa984f28bf7b31
AstralCreator <astralcreationllc@protonmail.com>
2026-05-14 00:45:02 -0500

    modifying start server for global inserts

--- scripts/start_server.ps1
+++ scripts/start_server.ps1
@@ -7,6 +7,13 @@ $ConfigPath = "$Root\config\config.local.json"
 
 $Config = Get-Content $ConfigPath -Raw | ConvertFrom-Json
 
+if ($Config.server_name -eq "eu-wheel") {
+    Write-Host "Starting Global Collector"
+    Start-Process "$Root\python-embed\App\Python\python.exe" `
+        -ArgumentList "$Root\global\collector.py" `
+        -WorkingDirectory "$Root"
+}
+
 Write-Host "Starting server: $($Config.server_name)"
 
 Start-Process $Python `
